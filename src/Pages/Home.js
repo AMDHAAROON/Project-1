d2259fbdd741e767bded647e9c556ac00c0f3b3d
AMDHAAROON <mdhaaroon2020@gmail.com>
2025-03-07 22:05:30 +0530

    Refactor Home component layout to use Box for better structure and add padding for main content

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,18 +1,21 @@
 import React from "react";
 import Navbar from "../Component/Navbar";
-import Footer from "../Component/Footer"
+import Footer from "../Component/Footer";
 import { Box } from "@mui/material";
+
 const Home = () => {
   return (
-    <>
+    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
       <Box sx={{ bgcolor: "#000212" }}>
-       
         <Navbar color="#fff" />
       </Box>
-      <Box sx={{pt:'10' }}>
-      <Footer/>
+      <Box sx={{ flex: 1, pt: 10 }}> {/* Add padding top */}
+        {/* Main content goes here */}
       </Box>
-    </>
+      <Box>
+        <Footer />
+      </Box>
+    </Box>
   );
 };
 
